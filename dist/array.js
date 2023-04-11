b39b952cb57f85e0810b887bb4efeac3ae71f34c
Sakib Siddiqi Supto <sakibsiddiqi15@gmail.com>
2023-04-11 17:56:38 +0600

    :- Array

--- dist/array.js
+++ dist/array.js
@@ -1,5 +1,10 @@
 var names = [];
 names.push("Sakib");
+/** Readonly */
+var other_names = [];
+// other_names.push("12"); //?Error: Argument of type 'string' is not assignable to parameter of type 'number'.
 var ages = [];
-ages.push("12");
-ages.push(12);
+ages.push(21);
+ages.push("21");
+//ages.push(true); //?Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
+console.log(ages);
--- src/array.ts
+++ src/array.ts
@@ -1,6 +1,13 @@
 const names: string[] = [];
 names.push("Sakib");
 
-const ages: string[] | number[] = [];
-ages.push("12");
-ages.push(12);
+/** Readonly */
+const other_names:readonly string[]=[];
+// other_names.push("12"); //?Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+
+
+const ages:(string|number)[]=[];
+ages.push(21);
+ages.push("21");
+//ages.push(true); //?Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
+console.log(ages);
